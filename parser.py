68ea361372358e7e6ed2bc887ef1f0fb8153dda4
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-10 09:36:19 +0100

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,19 @@
+
+import os, pandas, csv, re
+import numpy as np
+from biothings.utils.dataload import dict_convert, dict_sweep
+from biothings import config
+logging = config.logger
+def load_Refseq_gene(data_folder):
+    infile = os.path.abspath("/opt/biothings/GRCh37/refseq_genes/r105/GeneInterval.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
+    results = {}
+    for rec in dat:
+        _id = rec["refseqID"]
+        process_key = lambda k: k.replace(" ","_").lower()
+		rec = dict_convert(rec,keyfn=process_key)
+		results.setdefault(_id,[]).append(rec)
+    for _id,docs in results.items():
+        doc = {"_id": _id, "Refseq_gene" : docs}
+        yield doc
